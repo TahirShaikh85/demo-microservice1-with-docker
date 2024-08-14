45d9bd90fdcbcc1e7c2f0d12017973e3c2b2aafb
tahirshaikh85 <tahirshaikhrs7@gmail.com>
2024-08-14 12:39:50 +0530

    fixed route issue in server.js

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express();
+const PORT = process.env.PORT
 
 app.use(express.json())
 
-app.route('/',(req,res)=>{
-    return res.status(200).send("Hello World");
+app.get('/', (req, res) => {
+    return res.status(200).send("Hello... this server is setup using GitHub actions (CI/CD pipeline) and deployed using docker + AWS EC2");
 });
 
-app.listen(process.env.PORT,()=>{
-    console.log(`Server is running on http://localhost:${process.env.PORT} made by - ${process.env.DEVELOPER}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 })
